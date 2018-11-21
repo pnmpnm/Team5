4025930ba0a890330e8666150440de0da437850c
Ponomarev Aleksandr <pnm1207@gmail.com>
2018-11-21 23:13:15 +0200

    Home work4

--- 07/days/JS/app.js
+++ 07/days/JS/app.js
@@ -1,35 +1,35 @@
 'use strict';
 
-let первый = 'Понедельник';
-let второй = 'Вторник';
-let третий = 'Среда';
-let четвертый = 'Четверг';
-let пятый = 'Пятница';
-let шестой = 'Суббота';
-let седьмой = 'Воскресенье';
+let one = 'Понедельник';
+let two = 'Вторник';
+let three = 'Среда';
+let four = 'Четверг';
+let five = 'Пятница';
+let six = 'Суббота';
+let seven = 'Воскресенье';
 let daynum = prompt('Номер дня', 1);
 
 switch (daynum){
     case '1':
-        alert(первый);
+        alert(one);
         break;
     case '2':
-        alert(второй);
+        alert(two);
         break;
     case '3':
-        alert(третий);
+        alert(three);
         break;
     case '4':
-        alert(четвертый);
+        alert(four);
         break;
     case '5':
-        alert(пятый);
+        alert(five);
         break;
     case '6':
-        alert(шестой);
+        alert(six);
         break;
     case '7':
-        alert(седьмой);
+        alert(seven);
         break;
     default:alert("Такого дня недели не существует!");
 }
